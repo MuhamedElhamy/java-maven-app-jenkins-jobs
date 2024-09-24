1c40eb13f0d68eb39c3a70e66d724ecf87d124f6
Muhamed Elhamy Muhamed <138899142+MuhamedElhamy@users.noreply.github.com>
2024-09-24 07:05:05 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,34 @@
-def buildApp() {
-    echo "Building the app..."
-}
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn clean package'
+} 
 
-def testApp() {
-    echo "Testing the app..."
-}
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t mhmdelhamy/java-maven-app:${IMAGE_NAME} .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push mhmdelhamy/java-maven-app:${IMAGE_NAME}'
+    }
+} 
 
 def deployApp() {
-    echo "Deploying the app..."
-    echo "Deploying version ${params.VERSION}"
+    echo 'deploying the application...'
+}
+
+def versionUpdate() {
+    echo 'commit version update...'
+    withCredentials([usernamePassword(credentialsId: 'gitlab-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        // git config here for the first time run
+        sh 'git config --global user.email "jenkins@example.com"'
+        sh 'git config --global user.name "jenkins"'
+    
+        sh "git remote set-url origin  https://$USER:$PASS@gitlab.com/devops7303738/java-maven-app.git"
+        sh 'git add .'
+        sh 'git commit -m "ci: version bump"'
+        sh 'git push origin HEAD:master'
+    }
+
 }
 
 return this
